06a8eb520e921b3f8b457fc04944c90c150f7d27
fbriantii <aunthyy@gmail.com>
2024-09-26 11:32:47 +0700

    login

---
+++ resources/views/template/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login-container">
+        <form action="">
+            <label for="Username">Username</label>
+            <input type="text" name="username">
+            <label for="Password">Password</label>
+            <input type="password" name="password">
+            <button class="btn btn-success">Login</button>
+        </form>
+    </div>
+</body>
+</html>
