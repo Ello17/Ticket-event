e60b99580c0b5e4cee9857cac35e37a98cc18efe
Ello17 <azzellofabian@gmail.com>
2024-10-02 16:09:21 +0700

    --

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -9,12 +9,12 @@
             <a href="#"><i class="ri-calendar-event-fill"></i> Event</a>
         </li>
         <li>
-            <a href="#"><i class="ri-user-line"></i></a>
+            <a href="{{route('login')}}"><i class="ri-user-line"></i></a>
         </li>
     </ul>
 </div>
 <div>
-    <button class="bg-[#7fa1c3] text-white px-5 py-2">Log In</button>
+    <button class="bg-[#7fa1c3] text-white px-5 py-2">Log In </button>
 </div>
 </nav>
 </header>
