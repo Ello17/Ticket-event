6e1ba0ae990013055f2f7b2692f931b3da9fec85
fbriantii <aunthyy@gmail.com>
2024-10-02 10:14:18 +0700

    udah bisa login

--- resources/views/template/login.blade.php
+++ resources/views/template/login.blade.php
@@ -12,35 +12,40 @@
 
   <!--==================== LOGIN ====================-->
   <div class="login" id="login">
-    <form action="{{route ('postLogin')}}" class="login__form" method="POST">
-        @csrf
-       <h2 class="login__title">Log In</h2>
-
-       <div class="login__group">
+   <form action="{{ route('postLogin') }}" class="login__form" method="POST">
+      @csrf
+      <h2 class="login__title">Log In</h2>
+  
+      <div class="login__group">
           <div>
-             <label for="email" class="login__label">Email</label>
-             <input type="email" placeholder="Write your email" id="email" class="login__input">
+              <label for="email" class="login__label">Email</label>
+              <input type="email" name="email" placeholder="Write your email" id="email" class="login__input">
           </div>
-
+  
           <div>
-             <label for="password" class="login__label">Password</label>
-             <input type="password" placeholder="Enter your password" id="password" class="login__input">
+              <label for="password" class="login__label">Password</label>
+              <input type="password" name="password" placeholder="Enter your password" id="password" class="login__input">
           </div>
-       </div>
-
-       <div>
+      </div>
+  
+      <div>
           <p class="login__signup">
-             You do not have an account? <a href="#">Sign up</a>
+              You do not have an account? <a href="{{ route('register') }}">Sign up</a>
           </p>
-
+  
           <a href="#" class="login__forgot">
-             You forgot your password
+              You forgot your password
           </a>
-
+  
+          @if (Session::has('notifikasi'))
+              <p class="error-message">{{ Session::get('notifikasi') }}</p>
+          @endif
+  
           <button type="submit" class="login__button">Log In</button>
-       </div>
-    </form>
-
+      </div>
+  </form>
+  
+  
     <i class="ri-close-line login__close" id="login-close"></i>
  </div>
 
