0b5d5f3ededbc6583a317f85368e0b495baf4e80
Rchyy <ayialipa16@gmail.com>
2024-10-02 14:55:58 +0700

    ngerapihin

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
-
-//ini buat customer
-Route::get('/homeCustomer', [CustomerController::class, 'homeCustomer'])->name('homeCustomer');
-
-
-// ini route buat auth bisi gabisa pake yang ini berarti pake yang []
-Route::get('/login', 'AuthController@login')->name('login');
-Route::post('/postLogin', 'AuthController@postLogin')->name('postlogin');
-
-//ini buat customer
-Route::get('/homeCustomer', 'CustomerController@homeCustomer')->name('homeCustomer');
 // Register
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/postRegister', [AuthController::class, 'postRegister'])->name('postRegister');
@@ -40,34 +28,19 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 
 
 // ini buat customer
-Route::get('/', [CustomerController::class, 'home'])->name('home');
+Route::get('/homeCustomer', 'CustomerController@homeCustomer')->name('homeCustomer');
 Route::get('/detail-event/{id}', [CustomerController::class, 'detailEvent'])->name('detailEvent');
 Route::get('/list-events', [CustomerController::class, 'listEvents'])->name('listEvent');
 
-//admin
-Route::get('/homeAdmin', 'AdminController@homeAdmin')->name('homeAdmin');
-
-//kelolauser
-Route::get('/kelolaUser', 'AdminController@kelolaUser')->name('kelolaUser');
-
-//list
-Route::get('/admin/edit-list/{id}', [App\Http\Controllers\AdminController::class, 'editList'])->name('admin.editList');
-Route::post('/posteditlist/{id}', [AdminController::class, 'posteditlist'])->name('posteditlist');
-Route::get('/hapusList/{id}', [AdminController::class, 'hapusList'])->name('hapusList');
-// Route::get('/detailList/{id}', [AdminController::class, 'detailList'])->name('detailList');
-// Route::middleware('auth')->group(function () {
-
-
 
 Route::middleware('auth')->group(function () {
+    Route::get('/homeAdmin', 'AdminController@homeAdmin')->name('homeAdmin');
+    Route::get('/kelolaUser', 'AdminController@kelolaUser')->name('kelolaUser');
+    Route::get('/admin/edit-list/{id}', [AdminController::class, 'editList'])->name('admin.editList');
+    Route::post('/posteditlist/{id}', [AdminController::class, 'posteditlist'])->name('posteditlist');
+    Route::get('/hapusList/{id}', [AdminController::class, 'hapusList'])->name('hapusList');
 
-//ini buat admin
-// Route::get('/homeAdmin', 'AdminController@homeAdmin')->name('homeAdmin');
 
-//ini buat creator
-// Route::get('/homeCreator', 'CreatorController@homeCreator')->name('homeCreator');
 
 });
 
-
-ditEvent');
