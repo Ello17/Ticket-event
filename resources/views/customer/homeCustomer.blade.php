a62f1067e340102919920e99062a0b908eb7a1d6
Rchyy <ayialipa16@gmail.com>
2024-10-02 10:39:22 +0700

    seeder

--- resources/views/customer/homeCustomer.blade.php
+++ resources/views/customer/homeCustomer.blade.php
@@ -5,7 +5,7 @@
     @section('title', 'Tiket Mudah hanya di Tiket Mudah')
     @section('content')
     <section>
-        
+
     </section>
 
     @endsection
@@ -118,5 +118,5 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
-</html> 
+</html>
 
