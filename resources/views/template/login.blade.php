5ccbcadbf317eeb3a33c90a068ef6a469c3099c0
Ello17 <azzellofabian@gmail.com>
2024-10-02 10:41:47 +0700

    --

--- resources/views/template/login.blade.php
+++ resources/views/template/login.blade.php
@@ -1,55 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
+@extends('layouts.app2')
+@push('css')
+<link rel="stylesheet" href="{{asset('components/css/register.css')}}">
+@endpush
+    @section('title', 'Sign Up')
+    @section('content')
+
+    <div class="container">
+        <div class="box-image">
+            <img src="{{asset('components/asset/logo/512.png')}}" alt="">
+        </div>
+        <div class="box-form">
+            <h2>Login</h2>
+            <form action="{{ route('postLogin') }}" class="login_form" method="POST">
+                @csrf
+                <div class="register-group">
+                    <div>
+                        <label for="email" class="login_label">Email</label>
+                        <input type="email" name="email" placeholder="Write your email" id="email" class="login_input">
+                    </div>
+                    <div>
+                        <label for="password"  class="login_label">Password</label>
+                        <input type="password" name="password" placeholder="Enter your password" id="password" class="login_input">
+                    </div>
+                </div>
+                <div>
+                    @if (Session::has('notifikasi'))
+                    <p class="error-message">{{ Session::get('notifikasi') }}</p>
+                @endif
+
+                <button type="submit" class="login_button">Log In</button>
+                </div>
+            </form>
+            <p class="signup_login">
+                       You do not have an account? <a href="{{ route('register') }}">Sign up</a>
+            </p>
+        </div>
+    </div>
 
 
-
-  <!--==================== LOGIN ====================-->
-  <div class="login" id="login">
-   <form action="{{ route('postLogin') }}" class="login__form" method="POST">
-      @csrf
-      <h2 class="login__title">Log In</h2>
-  
-      <div class="login__group">
-          <div>
-              <label for="email" class="login__label">Email</label>
-              <input type="email" name="email" placeholder="Write your email" id="email" class="login__input">
-          </div>
-  
-          <div>
-              <label for="password" class="login__label">Password</label>
-              <input type="password" name="password" placeholder="Enter your password" id="password" class="login__input">
-          </div>
-      </div>
-  
-      <div>
-          <p class="login__signup">
-              You do not have an account? <a href="{{ route('register') }}">Sign up</a>
-          </p>
-  
-          <a href="#" class="login__forgot">
-              You forgot your password
-          </a>
-  
-          @if (Session::has('notifikasi'))
-              <p class="error-message">{{ Session::get('notifikasi') }}</p>
-          @endif
-  
-          <button type="submit" class="login__button">Log In</button>
-      </div>
-  </form>
-  
-  
-    <i class="ri-close-line login__close" id="login-close"></i>
- </div>
-
-
-
-</body>
-</html>
+    @endsection
+@push('js')
+@endpush
