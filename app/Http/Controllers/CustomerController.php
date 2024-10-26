b9fcbe0e11eec1553d3253fe3fe82b27d035ad71
Rchyy <ayialipa16@gmail.com>
2024-10-26 11:04:44 +0700

    -

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use App\Models\Tiket;
-use App\Models\Transaksi;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -23,15 +22,8 @@ class CustomerController extends Controller
 
     }
 
-    public function history()
-    {
-        $userId = Auth::id();
-        $transaksiList = Transaksi::whereHas('tiket', function ($query) use ($userId) {
-            $query->whereHas('event', function ($query) use ($userId) {
-                $query->where('user_id', $userId);
-            });
-        })->get();
-        return view('customer.history', compact('transaksiList'));
+    function history() {
+        return view('customer.history');
     }
     public function detailEvent($id)
     {
@@ -146,7 +138,6 @@ public function transaksi($id, Tiket $tiket, Request $request)
 {
     $event = Event::find($id);
     $tiket = Tiket::where('event_id', $id)->first();
-    $user = Auth::user();
 
         $tiket_dibeli = $request->input('tiket_dibeli');
         $total_harga = $tiket->harga_tiket * $tiket_dibeli;
@@ -178,6 +169,7 @@ public function transaksi($id, Tiket $tiket, Request $request)
         return redirect()->back()->withErrors('Event tidak ditemukan.');
     }
 
-        return view('customer.transaksi', compact('event', 'tiket',  'formatted_total_harga', 'tiket_dibeli', 'snapToken', 'user'));
+        return view('customer.transaksi', compact('event', 'tiket',  'formatted_total_harga', 'tiket_dibeli', 'snapToken'));
         }
 }
+
