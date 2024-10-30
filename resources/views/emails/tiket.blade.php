d5235669851f403f00de119b3fc83cc06ece563f
Ello17 <azzellofabian@gmail.com>
2024-10-30 13:41:03 +0700

    --

--- resources/views/emails/tiket.blade.php
+++ resources/views/emails/tiket.blade.php
@@ -4,11 +4,11 @@
     <title>Tiket Event</title>
 </head>
 <body>
-    <h1>Tiket Event</h1>
+    <h1 style="text-align: center;">Tiket Event</h1>
     <p>Kode Tiket: {{ $transaksi->kode_tiket }}</p>
 
-    <div>
-        {!! DNS1D::getBarcodeHTML($transaksi->kode_tiket, 'C39') !!}
+    <div class="code">
+        {!! DNS1D::getBarcodeHTML($transaksi->kode_tiket, 'C39',1,23,'green') !!}
     </div>
 </body>
 </html>
