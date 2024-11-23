fdc279626d2cd85c8594dd46a6808a62975b7ed9
fbriantii <aunthyy@gmail.com>
2024-11-23 10:35:12 +0700

    tabel parti

--- app/Models/participant.php
+++ app/Models/participant.php
@@ -5,31 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Participant extends Model
+class participant extends Model
 {
     use HasFactory;
     
     protected $fillable = [
-        'user_id',
-        'event_id',
-        'tiket_id',
-        'kode_tiket',
-        'scan_time',
-        'is_present',
+        'kode_result',
+        'status',
     ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function event()
-    {
-        return $this->belongsTo(Event::class);
-    }
-
-    public function tiket()
-    {
-        return $this->belongsTo(Tiket::class);
-    }
 }
--- database/migrations/2024_11_13_041843_create_participants_table.php
+++ database/migrations/2024_11_13_041843_create_participants_table.php
@@ -14,13 +14,9 @@ class CreateParticipantsTable extends Migration
     public function up()
     {
         Schema::create('participants', function (Blueprint $table) {
-            $table->id(); 
-            $table->foreignId('user_id')->constrained(); 
-            $table->foreignId('event_id')->constrained();
-            $table->foreignId('tiket_id')->constrained(); 
-            $table->string('kode_tiket')->unique(); 
-            $table->timestamp('scan_time')->nullable(); 
-            $table->boolean('is_present')->default(false); 
+            $table->id();
+            $table->string('kode_result')->unique();
+            $table->string('status');
             $table->timestamps();
         });
     }
